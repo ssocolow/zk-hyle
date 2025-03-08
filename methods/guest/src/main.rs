9f87c02c2d09dbd16899cbfc38e0683670109e88
Simon Socolow <simonsocolow78@gmail.com>
2025-03-08 22:47:27 +0000

    Swap template

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -3,7 +3,7 @@
 
 extern crate alloc;
 
-use contract_identity::IdentityContractState;
+use contract::Counter;
 use sdk::guest::execute;
 use sdk::guest::GuestEnv;
 use sdk::guest::Risc0Env;
@@ -11,8 +11,13 @@ use sdk::guest::Risc0Env;
 risc0_zkvm::guest::entry!(main);
 
 fn main() {
+    //
+    // Usually you don't need to update this file.
+    // Except to specify the name of your contract type (here = Counter)
+    //
+
     let env = Risc0Env {};
     let input = env.read();
-    let (_, output) = execute::<IdentityContractState>(&input);
+    let (_, output) = execute::<Counter>(&input);
     env.commit(&output);
 }
